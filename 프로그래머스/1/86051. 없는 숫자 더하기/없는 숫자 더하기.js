49e4c63295f1c92840eeb56730a6d0785d207a43
김규리 <115445005+lamiiiii@users.noreply.github.com>
2025-05-05 18:46:38 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,15 +1,3 @@
-function solution(numbers) {
-    const array = [false];
-    let sum = 0;
-    
-    for(num of numbers){
-        array[num] = true;
-    }
-    
-    for(i=0; i<10; i++){
-        if(!array[i]){
-            sum += i;
-        }
-    }
-    return sum;
+function solution(numbers){
+    return 45 - numbers.reduce((a, b) => a + b, 0);
 }
